e7c66f0386051b48082fa1b50b5878acf3e67b99
Michael O'Brien <michael@obrienlabs.org>
2016-12-26 23:28:21 -0500

    #1 20131102 eclipselink orm

--- Biometric/BMViewController.h
+++ Biometric/BMViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreBluetooth/CoreBluetooth.h>
+#import <CoreLocation/CoreLocation.h>
 
 //@interface BMViewController : UIViewController
 @interface BMViewController : UIViewController <CBCentralManagerDelegate, CBPeripheralDelegate>
@@ -16,21 +17,68 @@
 //@property (nonatomic, strong) CBMutableCharacteristic *customCharacteristic;
 @property (weak, nonatomic) IBOutlet UITextField *deviceTextField;
 //@property (nonatomic, strong) CBMutableService *customService;
-@property (weak, nonatomic) IBOutlet UITextView *statusTextView;
+//@property (weak, nonatomic) IBOutlet UITextView *statusTextView;
 //@property (weak, nonatomic) IBOutlet UITextField *cloudField;
 //@property (weak, nonatomic) IBOutlet UITextField *uuidField;
 @property (weak, nonatomic) IBOutlet UIButton *readButton;
+@property (weak, nonatomic) IBOutlet UITextField *maxRate2TextField;
+@property (weak, nonatomic) IBOutlet UITextField *minRate2TextField;
+@property (weak, nonatomic) IBOutlet UITextField *device2TextField;
+@property (weak, nonatomic) IBOutlet UISwitch *uploadUISwitch;
+@property (weak, nonatomic) IBOutlet UITextField *uploadConterTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *rate2TextView;
+
+//@property (weak, nonatomic) IBOutlet UITextView *uploadsTextView;
+
+// Core Location
+@property (weak, nonatomic) IBOutlet UITextField *longTextField;
+@property (weak, nonatomic) IBOutlet UITextField *latTextField;
+@property (weak, nonatomic) IBOutlet UITextField *altTextField;
+
 
 - (IBAction)readTouchDown:(id)sender;
 //@property (weak, nonatomic) IBOutlet UILabel *uuidLabel;
 
-@property (nonatomic, strong) CBPeripheral *peripheral;
+@property (nonatomic, strong) CBPeripheral *peripheral1;
+@property (nonatomic, strong) CBPeripheral *peripheral12;
 @property (nonatomic, strong) CBCharacteristic *aCharacteristic;
 @property (nonatomic, strong) CBCentralManager *manager;
-@property (nonatomic, strong) NSMutableData *data;
-@property (assign) uint16_t heartRate;
-@property (assign) uint16_t heartRateMax;
-@property (assign) uint16_t heartRateMin;
+//@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (weak, nonatomic) IBOutlet UITextField *bearingTextField;
+
+@property (nonatomic, strong) NSMutableData *data1;
+@property (nonatomic, strong) NSMutableData *data2;
+@property (assign) uint16_t heartRate1;
+@property (assign) uint16_t lastHeartRate1;
+@property (assign) uint16_t lastHeartRate2;
+@property (assign) uint16_t heartRateMax1;
+@property (assign) uint16_t heartRateMin1;
+@property (assign) uint16_t heartRate2;
+@property (assign) uint16_t heartRateMax2;
+@property (assign) uint16_t heartRateMin2;
+
+@property (assign) double lastHeading;
+@property (assign) double currHeading;
+@property (weak, nonatomic) IBOutlet UITextField *accelXtextField;
+@property (weak, nonatomic) IBOutlet UITextField *accelYtextField;
+@property (weak, nonatomic) IBOutlet UITextField *accelZtextField;
+@property (weak, nonatomic) IBOutlet UITextField *maxAccelXtextField;
+@property (weak, nonatomic) IBOutlet UITextField *maxAccelYtextField;
+@property (weak, nonatomic) IBOutlet UITextField *maxAccelZtextField;
+@property (weak, nonatomic) IBOutlet UITextField *rotXtextField;
+@property (weak, nonatomic) IBOutlet UITextField *rotYtextField;
+@property (weak, nonatomic) IBOutlet UITextField *rotZtextField;
+@property (weak, nonatomic) IBOutlet UITextField *maxRotXtextField;
+@property (weak, nonatomic) IBOutlet UITextField *maxRotYtextField;
+@property (weak, nonatomic) IBOutlet UITextField *maxRotZtextField;
+
+// http://nscookbook.com/2013/03/ios-programming-recipe-19-using-core-motion-to-access-gyro-and-accelerometer/
+
+
+
+
+- (IBAction)resetMaxValues:(id)sender;
 
 //-(IBACTION)DISmissKeyboardOnTap:(id)sender;
 @end
--- Biometric/BMViewController.m
+++ Biometric/BMViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "BMViewController.h"
+// https://developer.apple.com/library/ios/documentation/userexperience/conceptual/LocationAwarenessPG/CoreLocation/CoreLocation.html
+#import <CoreLocation/CoreLocation.h>
+#import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
 
 @interface BMViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *ratePeakField;
@@ -15,6 +19,28 @@
 @property (weak, nonatomic) IBOutlet UITextField *rateField;
 @property (weak, nonatomic) IBOutlet UITextField *statusField;
 @property (weak, nonatomic) IBOutlet UITextField *statusTextField;
+
+//@property (nonatomic, strong) CLLocationManager *locationManager;
+// https://developer.apple.com/library/ios/documentation/CoreMotion/Reference/CoreMotion_Reference/CoreMotion_Reference.pdf
+// https://developer.apple.com/library/ios/documentation/EventHandling/Conceptual/EventHandlingiPhoneOS/motion_event_basics/motion_event_basics.html#//apple_ref/doc/uid/TP40009541-CH6-SW4
+@property (nonatomic, strong) CMMotionManager *motionManager;
+@property(readonly, nonatomic) CMAcceleration acceleration;
+
+
+
+@property(readonly) CMAccelerometerData *accelerometerData;
+// roll
+// pitch
+// yaw
+// rotationMatrix
+// quaternion
+// attitude
+// rotationRate
+// gravity
+// userAcceleration
+// magneticField
+// timestamp
+// motionActivity (M7 chip in 5s - stationary/running/walking/automotive/unknown, startdate/confidence
 @end
 
 @implementation BMViewController
@@ -30,14 +56,237 @@ static NSString * const kServiceUUID_wahoo = @"4A90672B-EC3A-BEC2-5833-AD5A559DE
 static NSString * const HEARTRATE_UUID = @"2a37";
 static NSString * const cloudURLString = @"https://obrienscience-obrienlabs.java.us1.oraclecloudapps.com/gps/FrontController?action=setGps";//&u=20131027&lt=0&lg=0&al=0&hr=999
 static int uploads = 0;
-static int uploadFlag = 1;
-static NSString * const USER_ID = @"20131031";
+//static int uploadFlag = 1;
+static NSString * const USER_ID = @"20131102";
+
+CLLocationManager *locationManager;// = [[CLLocationManager alloc] init];
+double currentMaxAccelX;
+double currentMaxAccelY;
+double currentMaxAccelZ;
+double currentMaxRotX;
+double currentMaxRotY;
+double currentMaxRotZ;
+
+- (void)startSignificantChangeUpdates
+{
+     // Create the location manager if this object does not
+     // already have one.
+     if (nil == locationManager)
+          locationManager = [[CLLocationManager alloc] init];
+     
+     locationManager.delegate = self;
+     [locationManager startMonitoringSignificantLocationChanges];
+}
+
+// Delegate method from the CLLocationManagerDelegate protocol.
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+     // If it's a relatively recent event, turn off updates to save power.
+     CLLocation* location = [locations lastObject];
+     NSDate* eventDate = location.timestamp;
+     NSTimeInterval howRecent = [eventDate timeIntervalSinceNow];
+     if (abs(howRecent) < 15.0) {
+          // If the event is recent, do something with it.
+          //NSLog(@"latitude %+.6f, longitude %+.6f\n",
+          //      location.coordinate.latitude,
+          //      location.coordinate.longitude);
+          NSString *latString = [NSString stringWithFormat:@"%+.4f", location.coordinate.latitude];
+          NSString *lonString = [NSString stringWithFormat:@"%+.4f", location.coordinate.longitude];
+          self.longTextField.text = lonString;
+          self.latTextField.text = latString;
+     }
+}
+
+- (void)startStandardUpdates
+{
+     // Create the location manager if this object does not
+     // already have one.
+     if (nil == locationManager)
+          locationManager = [[CLLocationManager alloc] init];
+     
+     locationManager.delegate = self;
+     locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+     
+     // Set a movement threshold for new events.
+     locationManager.distanceFilter = 5;//500; // meters
+     
+     [locationManager startUpdatingLocation];
+}
+
+
+
+// heading
+- (void)startHeadingEvents {
+     //if (!self.locationManager) {
+          //CLLocationManager* theManager = [[[CLLocationManager alloc] init] autorelease];
+          
+          // Retain the object in a property.
+          //self.locManager = theManager;
+          //locationManager.delegate = self;
+     //}
+     
+     // Start location services to get the true heading.
+     locationManager.distanceFilter = 1000;
+     locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+     [locationManager startUpdatingLocation];
+     
+     // Start heading updates.
+     if ([CLLocationManager headingAvailable]) {
+          locationManager.headingFilter = 5;
+          [locationManager startUpdatingHeading];
+     }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading {
+     if (newHeading.headingAccuracy < 0)
+          return;
+     
+     // Use the true heading if it is valid.
+     CLLocationDirection  theHeading = ((newHeading.trueHeading > 0) ?
+                                        newHeading.trueHeading : newHeading.magneticHeading);
+     self.lastHeading = self.currHeading;
+     self.currHeading = theHeading;
+          if(self.lastHeading < self.currHeading) {
+               self.bearingTextField.backgroundColor = [UIColor greenColor];
+               [self.bearingTextField setTextColor: [UIColor blackColor]];
+          } else {
+          if(self.lastHeading == self.currHeading) {
+               self.bearingTextField.backgroundColor = [UIColor yellowColor];
+               [self.bearingTextField setTextColor: [UIColor blackColor]];
+          } else {
+               self.bearingTextField.backgroundColor = [UIColor redColor];
+               [self.bearingTextField setTextColor: [UIColor whiteColor]];
+          }}
+     NSString *headingString = [NSString stringWithFormat:@"%+.1f", theHeading];
+     
+     self.bearingTextField.text = headingString;
+     //[self updateHeadingDisplays];
+}
+
+/*- (void) toggleColor:(int) diff:UITextField control {
+     
+}*/
+// device motion
+
+// accelerometer
+
+
+// gyroscope
+
+static const NSTimeInterval gyroMin = 0.01;
+
+/*- (void)startUpdatesWithSliderValue:(int)sliderValue {
+     
+     // Determine the update interval
+     NSTimeInterval delta = 0.005;
+     NSTimeInterval updateInterval = gyroMin + delta * sliderValue;
+     
+
+// Create a CMMotionManager
+CMMotionManager *mManager = //[(APLAppDelegate *)[[UIApplication sharedApplication] delegate] sharedManager];
+APLGyroGraphViewController * __weak weakSelf = self;
+
+// Check whether the gyroscope is available
+if ([mManager isGyroAvailable] == YES) {
+     // Assign the update interval to the motion manager
+     [mManager setGyroUpdateInterval:updateInterval];
+     [mManager startGyroUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:^(CMGyroData *gyroData, NSError *error) {
+          [weakSelf.graphView addX:gyroData.rotationRate.x y:gyroData.rotationRate.y z:gyroData.rotationRate.z];
+          [weakSelf setLabelValueX:gyroData.rotationRate.x y:gyroData.rotationRate.y z:gyroData.rotationRate.z];
+     }];
+}
+self.updateIntervalLabel.text = [NSString stringWithFormat:@"%f", updateInterval];
+}
+
+- (void)stopUpdates{
+     CMMotionManager *mManager = [(APLAppDelegate *)[[UIApplication sharedApplication] delegate] sharedManager];
+     if ([mManager isGyroActive] == YES) {
+          [mManager stopGyroUpdates];
+     }
+}*/
+
+// magnetometer
+
+-(void)outputAccelertionData:(CMAcceleration)acceleration
+{
+     
+     
+     
+     self.accelXtextField.text = [NSString stringWithFormat:@" %.2fg",acceleration.x];
+     if(fabs(acceleration.x) > fabs(currentMaxAccelX))
+     {
+          currentMaxAccelX = acceleration.x;
+     }
+     self.accelYtextField.text = [NSString stringWithFormat:@" %.2fg",acceleration.y];
+     if(fabs(acceleration.y) > fabs(currentMaxAccelY))
+     {
+          currentMaxAccelY = acceleration.y;
+     }
+     self.accelZtextField.text = [NSString stringWithFormat:@" %.2fg",acceleration.z];
+     if(fabs(acceleration.z) > fabs(currentMaxAccelZ))
+     {
+          currentMaxAccelZ = acceleration.z;
+     }
+     
+     self.maxAccelXtextField.text = [NSString stringWithFormat:@" %.2f",currentMaxAccelX];
+     self.maxAccelYtextField.text = [NSString stringWithFormat:@" %.2f",currentMaxAccelY];
+     self.maxAccelZtextField.text = [NSString stringWithFormat:@" %.2f",currentMaxAccelZ];
+     
+     
+}
+-(void)outputRotationData:(CMRotationRate)rotation
+{
+     
+     self.rotXtextField.text = [NSString stringWithFormat:@" %.2fr/s",rotation.x];
+     if(fabs(rotation.x) > fabs(currentMaxRotX))
+     {
+          currentMaxRotX = rotation.x;
+     }
+     self.rotYtextField.text = [NSString stringWithFormat:@" %.2fr/s",rotation.y];
+     if(fabs(rotation.y) > fabs(currentMaxRotY))
+     {
+          currentMaxRotY = rotation.y;
+     }
+     self.rotZtextField.text = [NSString stringWithFormat:@" %.2fr/s",rotation.z];
+     if(fabs(rotation.z) > fabs(currentMaxRotZ))
+     {
+          currentMaxRotZ = rotation.z;
+     }
+     
+     self.maxRotXtextField.text = [NSString stringWithFormat:@" %.2f",currentMaxRotX];
+     self.maxRotYtextField.text = [NSString stringWithFormat:@" %.2f",currentMaxRotY];
+     self.maxRotZtextField.text = [NSString stringWithFormat:@" %.2f",currentMaxRotZ];
+}
 
 - (void)viewDidLoad
 {
      [super viewDidLoad];
      // Do any additional setup after loading the view, typically from a nib.
+     // start Bluetooth 4.0 LE
      self.manager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
+     // start Core Location
+     //locationManager = [[CLLocationManager alloc] init];
+     [self startStandardUpdates];
+     [self startHeadingEvents];
+     
+     // motion
+     self.motionManager = [[CMMotionManager alloc] init];
+     self.motionManager.accelerometerUpdateInterval = .2;
+     self.motionManager.gyroUpdateInterval = .2;
+     
+     [self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue currentQueue]
+                                              withHandler:^(CMAccelerometerData  *accelerometerData, NSError *error) {
+                                                   [self outputAccelertionData:accelerometerData.acceleration];
+                                                   if(error){
+                                                        
+                                                        NSLog(@"%@", error);
+                                                   }
+                                              }];
+     
+     [self.motionManager startGyroUpdatesToQueue:[NSOperationQueue currentQueue]
+                                     withHandler:^(CMGyroData *gyroData, NSError *error) {
+                                          [self outputRotationData:gyroData.rotationRate];
+                                     }];
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -48,7 +297,7 @@ static NSString * const USER_ID = @"20131031";
 
 
 - (IBAction)readButtonTouchUpInside:(id)sender {
-     [self.peripheral readValueForCharacteristic:self.aCharacteristic];
+     [self.peripheral1 readValueForCharacteristic:self.aCharacteristic];
      NSLog(@"reading");
      self.rateField.text = @"Reading...";
      //uploadFlag = 1 - uploadFlag;
@@ -64,8 +313,8 @@ static NSString * const USER_ID = @"20131031";
             self.rateField.text = @"0";
             //self.cloudField.text = cloudURLString;
             /*self.statusTextView.text = @"Discovering LE Bluetooth devices";*/
-            self.heartRateMax = 0;
-            self.heartRateMin = 255;
+            self.heartRateMax1 = 0;
+            self.heartRateMin1 = 255;
             break;
         default:
             NSLog(@"Central Manager did change state");
@@ -76,8 +325,8 @@ static NSString * const USER_ID = @"20131031";
      // Stops scanning for peripheral
      [self.manager stopScan];
      
-     if (self.peripheral != peripheral) {
-          self.peripheral = peripheral;
+     if (self.peripheral1 != peripheral) {
+          self.peripheral1 = peripheral;
           NSLog(@"Connecting to peripheral %@", peripheral);
           //self.statusTextView.text = peripheral.description;
           //self.statusField.text = peripheral.identifier.UUIDString;
@@ -93,11 +342,11 @@ static NSString * const USER_ID = @"20131031";
      NSLog(@"Connected to peripheral %@", peripheral);
      self.deviceTextField.text = peripheral.name;
      // Clears the data that we may already have
-     [self.data setLength:0];
+     [self.data1 setLength:0];
      // Sets the peripheral delegate
-     [self.peripheral setDelegate:self];
+     [self.peripheral1 setDelegate:self];
      // Asks the peripheral to discover the service
-     [self.peripheral discoverServices:@[  ]];
+     [self.peripheral1 discoverServices:@[  ]];
 }
 
 
@@ -156,21 +405,46 @@ static NSString * const USER_ID = @"20131031";
           //self.uuidField.text = dataString;
           // from apple
           [self extractHeartRate:characteristic.value];
-          NSString *rateString = [NSString stringWithFormat:@"%hu", self.heartRate];
+          NSString *rateString = [NSString stringWithFormat:@"%hu", self.heartRate1];
           self.rateField.text = rateString;
+          
+          self.rate2TextView.text = rateString;
           NSLog(@"size: %lu value:%@ %@ %@",(unsigned long)data.length, dataString, myString, rateString);
-          if(self.heartRate > self.heartRateMax) {
-               self.heartRateMax = self.heartRate;
+          if(self.heartRate1 > self.heartRateMax1) {
+               self.heartRateMax1 = self.heartRate1;
                self.ratePeakField.text = rateString;
+               self.maxRate2TextField.text = rateString;
           }
-          if(self.heartRate < self.heartRateMin) {
-               self.heartRateMin = self.heartRate;
+          if(self.heartRate1 < self.heartRateMin1) {
+               self.heartRateMin1 = self.heartRate1;
                self.rateMinField.text = rateString;
+               self.minRate2TextField.text = rateString;
           }
-          if(uploadFlag > 0) {
+          
+          if(self.lastHeartRate1 < self.heartRate1) {
+               self.rateField.backgroundColor = [UIColor greenColor];
+               [self.rateField setTextColor: [UIColor blackColor]];
+          }
+          if(self.lastHeartRate1 == self.heartRate1) {
+               self.rateField.backgroundColor = [UIColor yellowColor];
+               [self.rateField setTextColor: [UIColor blackColor]];
+          }
+          if(self.lastHeartRate1 > self.heartRate1) {
+               self.rateField.backgroundColor = [UIColor redColor];
+               [self.rateField setTextColor: [UIColor whiteColor]];
+          }
+          
+          
+          // get upload state from the switch
+          if(self.uploadUISwitch.isOn) {
+          //if(uploadFlag > 0) {
                [self httpPushToDataCenter];
+               self.uploadConterTextField.backgroundColor = [UIColor greenColor];
+               [self.uploadConterTextField setTextColor: [UIColor blackColor]];
           } else {
                //self.cloudField.text = @"Upload disabled";
+               self.uploadConterTextField.backgroundColor = [UIColor redColor];
+               [self.uploadConterTextField setTextColor: [UIColor whiteColor]];
           }
      }
 }
@@ -187,7 +461,7 @@ static NSString * const USER_ID = @"20131031";
      } else { // Notification has stopped
           // so disconnect from the peripheral
           NSLog(@"Notification stopped on %@.  Disconnecting", characteristic);
-          [self.manager cancelPeripheralConnection:self.peripheral];
+          [self.manager cancelPeripheralConnection:self.peripheral1];
      }
 }
 
@@ -203,8 +477,8 @@ static NSString * const USER_ID = @"20131031";
       self.heartRate = bpm;*/
      const uint8_t *reportData = [hrData bytes];
      uint16_t bpm = 0;
-     
      if ((reportData[0] & 0x01) == 0)
+     //if (null != reportData & (reportData[0] & 0x01) == 0)
      {
           /* uint8 bpm */
           bpm = reportData[1];
@@ -216,7 +490,8 @@ static NSString * const USER_ID = @"20131031";
      }
      
      //uint16_t oldBpm = self.heartRate;
-     self.heartRate = bpm;
+     self.lastHeartRate1 = self.heartRate1;
+     self.heartRate1 = bpm;
 }
 - (IBAction)rateDown:(id)sender {
      [[self view] endEditing:YES];
@@ -239,7 +514,7 @@ static NSString * const USER_ID = @"20131031";
      [url appendString: self.statusField.text];
      [url appendString: @"&pr=ios"];
      [url appendString: @"&hr="];
-     NSString *rateString = [NSString stringWithFormat:@"%hu", self.heartRate];
+     NSString *rateString = [NSString stringWithFormat:@"%hu", self.heartRate1];
      [url appendString: rateString];
      NSLog(@"Sending: %@",url);
      
@@ -259,11 +534,11 @@ static NSString * const USER_ID = @"20131031";
           //receivedData = nil;
           
           // Inform the user that the connection failed.
-          self.statusTextView.text = @"No connection";
+          self.uploadConterTextField.text = @"No connection";
      } else {
           uploads++;
           NSString *uploadsString = [NSString stringWithFormat:@"%i", uploads];
-          self.statusTextView.text = uploadsString;
+          self.uploadConterTextField.text = uploadsString;
      }
 }
 
